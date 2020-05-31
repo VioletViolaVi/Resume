01267ad3ce7e5ac8814b299f26cbea91f1a6c020
VioletViolaVi <vivian.v18723@gmail.com>
2020-05-31 13:46:12 +0000

    added ability to search for public github users

---
+++ assets/js/github-information.js
@@ -0,0 +1,52 @@
+function userInformationHTML(user){
+    return `<h2>
+              ${user.name}
+                <span class="small-name">
+                  (@<a href="${user.html_url}" target="_blank">
+                    ${user.login}
+                  </a>)
+                </span>
+            </h2>
+            <div class="gh-content">
+                <div class = "gh-avatar">
+                  <a href="${user.html_url} target="_blank">
+                  <img src="${user.avatar_url}" width="80" height="80" alt="${user.login}">
+                  </a>
+                </div>
+                <p>
+                  Followers: ${user.followers} - Following ${user.following} 
+                  <br> Repos: ${user.public_repos} 
+                </p>
+            </div>`;
+};
+
+function fetchGitHubInformation(event) {
+    let username = $("#gh-username").val();
+    if (!username) {
+        $("#gh-user-data").html(`<h2>Please enter a GitHub username</h2>`);
+        return;
+    };
+
+$("#gh-user-data").html(
+                       `<div id="loader">
+                          <img src="assets/css/loader.gif" alt="loading..."
+                        </div>`
+                       );
+                       
+$.when(
+    $.getJSON(`https://api.github.com/users/${username}`)
+).then(
+    function(response){
+        let userData = response;
+        $("#gh-user-data").html(userInformationHTML(userData));
+    }, function(errorResponse){
+        if(errorResponse.status === 4040){
+            $("#gh-user-data").html(`<h2>No info found for user ${username}</h2>`)
+        }else{
+            console.log(errorResponse);
+            $("#gh-user-data").html(
+                `<h2>Error: ${errorResponse.responseJSON.message}</h2>`);            
+        };
+    }
+)
+};
