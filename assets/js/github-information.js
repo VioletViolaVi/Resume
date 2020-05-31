dc3eee540ae2db3ed414df483e2a435fbc0f5002
VioletViolaVi <vivian.v18723@gmail.com>
2020-05-31 14:19:42 +0000

    fix bugs regarding empty div

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -40,6 +40,8 @@ function repoInformationHTML(repos){
 };
 
 function fetchGitHubInformation(event) {
+    $("#gh-user-data").html("");
+    $("#gh-repo-data").html("");
     let username = $("#gh-username").val();
     if (!username) {
         $("#gh-user-data").html(`<h2>Please enter a GitHub username</h2>`);
@@ -72,3 +74,5 @@ $.when(
     }
 )
 };
+
+$(document).ready(fetchGitHubInformation);
