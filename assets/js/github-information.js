d8553496dac41f776c1082b329b72609258ccec0
VioletViolaVi <vivian.v18723@gmail.com>
2020-05-31 17:07:01 +0000

    implemented emailJS integration on the contact page

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -64,11 +64,15 @@ $.when(
         $("#gh-user-data").html(userInformationHTML(userData));
         $("#gh-user-data").html(repoInformationHTML(repoData));
     }, function(errorResponse){
-        if(errorResponse.status === 4040){
-            $("#gh-user-data").html(`<h2>No info found for user ${username}</h2>`)
+        if(errorResponse.status === 404){
+            $("#gh-user-data").html(
+                `<h2>No info found for user ${username}</h2>`)
+        }else if(errorResponse.status === 403){
+            let resetTime = new Date(errorResponse.getResponseHeader('X-RateLimit-Reset')*1000);
+            $("#gh-user-data").html()
         }else{
             console.log(errorResponse);
-            $("#gh-user-data").html(
+            $("#gh-user-data").html(`<h4>Too manu requests, please wait until ${resetTime.toLocaleTimeString()}</h4>`
                 `<h2>Error: ${errorResponse.responseJSON.message}</h2>`);            
         };
     }
